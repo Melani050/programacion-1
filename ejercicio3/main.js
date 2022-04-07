8627380028548262b9ecbacfa11218cb920312b1
Melani050 <tina.meli25@gmail.com>
2022-04-07 17:08:37 -0300

    ejercicio3

---
+++ ejercicio3/main.js
@@ -0,0 +1,11 @@
+function calcular_circulo(radio)
+{
+    const resultado = Math.PI * Math.pow(radio,2)
+    return resultado
+}
+function mostrar()
+{
+    const radio = document.getElementById("inp_radio").value
+    const respuesta = calcular_circulo(radio)
+    document.getElementById("h_resultado").textContent = respuesta
+}
