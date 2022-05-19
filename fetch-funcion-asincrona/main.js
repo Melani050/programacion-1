1ca203c56aa7b4a24eb387e19e84a00806b10048
Melani050 <tina.meli25@gmail.com>
2022-05-19 14:32:29 -0300

    funcion fetch

--- fetch-funcion-asincrona/main.js
+++ fetch-funcion-asincrona/main.js
@@ -2,6 +2,7 @@ async function obtener_datos(){
     //fetch realiza una solicitud HTTP a través del metedo GET
     const datos = await fetch('https://jsonplaceholder.typicode.com/posts')   
     const datos_definitivos = await datos.json()
+    
     datos_definitivos.forEach(element => {
         console.log(element.title)
     });
