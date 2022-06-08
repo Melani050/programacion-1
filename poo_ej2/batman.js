ec00918642ecee0117cb195c1dc72d3be3be8857
Melani050 <tina.meli25@gmail.com>
2022-06-08 17:11:24 -0300

    for of- catalogo de peliculas

--- poo_ej2/batman.js
+++ poo_ej2/batman.js
@@ -6,14 +6,23 @@ export default class Batman {
     async consumir_api(){        
         const pelicula = await fetch('http://fake-movie-database-api.herokuapp.com/api?s=batman')
         const peliculas_definidas = await pelicula.json()
+        let columnas = []
         for (const Element of peliculas_definidas.Search) {
-            console.log(`
-                ${Element.imdbID}
-                ${Element.Title}
-                ${Element.Year}
-                ${Element.Poster}
-                `)
-        }
 
+            let columna =
+            `
+            <div class="col-lg-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="${Element.Poster}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${Element.Title}</h5>
+                        <p class="card-text">Estreno: ${Element.Year}</p>
+                    </div>
+                </div> 
+            </div>   
+            `
+            columnas.push(columna)
+        }
+        document.getElementById("div_row").innerHTML = columnas.join('')
     }
 }
