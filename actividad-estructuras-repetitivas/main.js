12ab2de92fcca90b1130a482a05680fbc9aeae20
Melani050 <tina.meli25@gmail.com>
2022-06-23 17:08:59 -0300

    actividad de repaso

---
+++ actividad-estructuras-repetitivas/main.js
@@ -0,0 +1,10 @@
+let numeros = [1,2,3,4,5,6,7,8,9,10]
+console.log(numeros)
+let numeritos = []
+for (const Element of numeros) {
+    let comprendidos = 
+    numeritos.push(comprendidos)
+  
+
+}
+console.log(numeritos)
